a10c058e69d6295f87437fe735261d730b11d6c4
lorenzocuoghi <42006664+lorenzocuoghi@users.noreply.github.com>
2020-04-14 20:52:23 +0200

    Hough transform to find lines

--- main.py
+++ main.py
@@ -10,19 +10,29 @@ while video.isOpened():
     frame = np.swapaxes(np.swapaxes(frame, 0, 2), 1, 2)  # (3, H, W)
     # frame = np.array(frame[::-1, :, :])  # from BGR to RGB
 
-    edge = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 50, 400)
-    contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Edge detection
+    edges = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 10, 1000, apertureSize=5, L2gradient=True)
 
-    maxH, maxW, maxX, maxY = None, None, None, None
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
-        if maxH is None or (h * w / 2) > (maxH * maxW / 2):
-            maxH, maxW, maxX, maxY = h, w, x, y
+    # Hough transform to find streight lines
+    lines = cv2.HoughLines(edges, 1.1, np.pi / 180, 200)
+    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+    for line in lines:
+        for rho, theta in line:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * a)
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * a)
 
-    cv2.rectangle(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (maxX, maxY), (maxX + maxW, maxY + maxH), (0, 255, 0), 2)
+            # cv2.line(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.line(edges, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    cv2.imshow('frame', np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2))
-    if cv2.waitKey(25) == ord('q'):
+    # cv2.imshow('frame', np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2))
+    cv2.imshow('frame', edges)
+    if cv2.waitKey(1000) == ord('q'):
         break
 
 video.release()
