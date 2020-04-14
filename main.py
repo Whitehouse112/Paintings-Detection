2193728726c22ac053cf94fd7f439f6a1db6ab0d
lorenzocuoghi <42006664+lorenzocuoghi@users.noreply.github.com>
2020-04-15 00:15:00 +0200

    ROI grossolana

--- main.py
+++ main.py
@@ -11,28 +11,24 @@ while video.isOpened():
     # frame = np.array(frame[::-1, :, :])  # from BGR to RGB
 
     # Edge detection
-    edges = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 10, 1000, apertureSize=5, L2gradient=True)
+    edges = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 10, 150, apertureSize=3, L2gradient=True)
 
-    # Hough transform to find streight lines
-    lines = cv2.HoughLines(edges, 1.1, np.pi / 180, 200)
-    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+    # Hough transform to find segments
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, minLineLength=100, maxLineGap=50)
+    img_lines = np.zeros_like(frame)
     for line in lines:
-        for rho, theta in line:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * a)
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * a)
+        x1, x2, y1, y2 = line[0, 0], line[0, 2], line[0, 1], line[0, 3]
+        cv2.line(np.swapaxes(np.swapaxes(img_lines, 0, 1), 1, 2), (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            # cv2.line(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.line(edges, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # Find single ROI (for every painting)
+    img_lines = cv2.cvtColor(np.swapaxes(np.swapaxes(img_lines, 0, 1), 1, 2), cv2.COLOR_RGB2GRAY)
+    contours, _ = cv2.findContours(img_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # cv2.imshow('frame', np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2))
-    cv2.imshow('frame', edges)
-    if cv2.waitKey(1000) == ord('q'):
+    cv2.imshow('frame', np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2))
+    if cv2.waitKey(1) == ord('q'):
         break
 
 video.release()
