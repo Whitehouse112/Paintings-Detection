e0de4a5e65f774dd79ec7417e37c1a64cc67f32a
Whitehouse112 <36161059+Whitehouse112@users.noreply.github.com>
2020-04-18 19:29:56 +0200

    Aggiornamento progetto
    - Aggiustato parametri Canny
    - Aggiunto istogramma ed entropia per riconoscimento quadri
    - cv2 imshow a quattro finestre

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
-# video = cv2.VideoCapture('VIRB0392.MP4')
-video = cv2.VideoCapture('GOPR5826.MP4')
+video = cv2.VideoCapture('VIRB0392.MP4')  # folder 000
+# video = cv2.VideoCapture('VIRB0407.MP4')  # folder 000
+# video = cv2.VideoCapture('GOPR5826.MP4')  # folder 001
 
 n_frame = 0
+frame_entr = 6.5
 while video.isOpened():
     ret, frame = video.read()
     if ret is False:
@@ -15,9 +18,9 @@ while video.isOpened():
     print("\nFrame", n_frame)
 
     # Edge detection
-    edges = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 10, 100, apertureSize=3, L2gradient=True)
+    edges = cv2.Canny(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), 10, 2000, apertureSize=5, L2gradient=True)
 
-    # Morphology transformation (closing)
+    # Morphqology transformation (closing)
     edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, (4, 4), iterations=1)
 
     # Significant contours
@@ -29,8 +32,8 @@ while video.isOpened():
     img_contours = cv2.cvtColor(np.swapaxes(np.swapaxes(img_contours, 0, 1), 1, 2), cv2.COLOR_RGB2GRAY)
     lines = cv2.HoughLinesP(img_contours, 1, np.pi / 180, 100, minLineLength=20, maxLineGap=10)
     img_lines = np.zeros_like(frame)
-    for line in lines:
-        x1, y1, x2, y2 = line[0, 0], line[0, 1], line[0, 2], line[0, 3]
+    for line in lines[:, 0]:
+        x1, y1, x2, y2 = line[0], line[1], line[2], line[3]
         cv2.line(np.swapaxes(np.swapaxes(img_lines, 0, 1), 1, 2), (x1, y1), (x2, y2), (0, 255, 0), thickness=2)
 
     # Morphology transformation (closing)
@@ -43,17 +46,32 @@ while video.isOpened():
     cv2.drawContours(np.swapaxes(np.swapaxes(img_contours, 0, 1), 1, 2), contours, -1, (0, 255, 0), thickness=2)
     img_contours = cv2.cvtColor(np.swapaxes(np.swapaxes(img_contours, 0, 1), 1, 2), cv2.COLOR_RGB2GRAY)
 
+    
     # Bounding boxes/ROIs
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
+    entropies = [frame_entr]
+    for cont in contours:
+        x, y, w, h = cv2.boundingRect(cont)
         if w > 100 and h > 100:
-            cv2.rectangle(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-            print((x, y, w, h))
 
-    cv2.imshow('ROIs', np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2))
-    # cv2.imshow('Edges', edges)
-    # cv2.imshow('Hough Lines', img_lines)
-    # cv2.imshow('Contours', img_contours)
+            roi = cv2.cvtColor(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), cv2.COLOR_RGB2GRAY)[y:y + h, x:x + w]
+
+            histo = np.float32((np.bincount(roi.ravel(), minlength=256)))
+            histo += 1
+            histo /= np.sum(histo)
+
+            entropy = -np.sum(histo * np.log2(histo))
+            
+            if entropy > frame_entr - 0.5:
+                entropies.append(entropy)
+                print(entropy, frame_entr)
+                cv2.rectangle(np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2), (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+                print((x, y, w, h))
+    frame_entr = np.sum(entropies) / len(entropies)
+
+    horizontal_concat_1 = np.concatenate((cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB), cv2.cvtColor(img_lines, cv2.COLOR_GRAY2RGB)), axis=1)
+    horizontal_concat_2 = np.concatenate((cv2.cvtColor(img_contours, cv2.COLOR_GRAY2RGB), np.swapaxes(np.swapaxes(frame, 0, 1), 1, 2)), axis=1)
+    vertical_concat = np.concatenate((horizontal_concat_1, horizontal_concat_2), axis=0)
+    cv2.imshow('ROIs', cv2.resize(vertical_concat, (1920,1080)))
     if cv2.waitKey(1) == ord('q'):
         break
 
